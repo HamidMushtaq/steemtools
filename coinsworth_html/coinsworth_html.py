859e6f907dec49a8c78e16165a9d231977ad6921
HamidMushtaq <hamidmushtaq@yahoo.com>
2018-01-29 09:50:30 +0100

    Now you can also set the currency. Currency should be set at the top of the coins.txt file (See coins.txt for example). Moreover, now changes in 1 and 24 hours are also shown. Furthermore, more crypto icons were added.

--- coinsworth_html/coinsworth_html.py
+++ coinsworth_html/coinsworth_html.py
@@ -9,13 +9,18 @@ import operator
 import datetime
 import webbrowser, os
 import os.path
+import locale
+
+CURRENCY="usd" # If no currency is given in coins.txt, USD would be assumed as default.
 
 class Coin(object):
-	def __init__(self, name, amount, price_usd):
+	def __init__(self, name, amount, price_cur, change1h, change24h):
 		self.name = name
 		self.amount = amount
-		self.price_usd = price_usd
-		self.worth = amount * price_usd
+		self.price_cur = price_cur
+		self.worth = amount * price_cur
+		self.change1h = change1h
+		self.change24h = change24h
 		
 def imageStr(coinInfo):
 	coinName = coinInfo.split(':')[0]
@@ -24,8 +29,16 @@ def imageStr(coinInfo):
 	else:
 		return ""
 		
+def getCurrencySymbol():
+	for currency in currencies:
+		print currency + '-' + CURRENCY
+		if currency == CURRENCY:
+			return currencies[CURRENCY]
+	return "$" # Dollar by default
+	
 market = coinmarketcap.Market()
-btc_usd = float(market.ticker("bitcoin")[0]["price_usd"])
+# If no currency is given in coins.txt, USD would be assumed as default
+btc_ratio = float(market.ticker("bitcoin", convert=CURRENCY)[0]["price_usd"]) 
 
 filepath = 'coins.txt'  
 coins = []
@@ -39,12 +52,18 @@ for line in lines:
 	if len(x) < 2:
 		continue
 	coinInfo = x[0].strip()
+	if coinInfo.lower() == "currency":
+		CURRENCY = x[1].strip().lower()
+		btc_ratio = float(market.ticker("bitcoin", convert=CURRENCY)[0]["price_" + CURRENCY])
+		continue
 	coinName = coinInfo.split(':')[0]
 	coinAmount = x[1].strip()
 	try:
-		coin = Coin(coinInfo, float(coinAmount), float(market.ticker(coinName)[0]["price_usd"]))  
+		coin = Coin(coinInfo, float(coinAmount), float(market.ticker(coinName, convert=CURRENCY)[0]["price_" + CURRENCY]), \
+			float(market.ticker(coinName)[0]["percent_change_1h"]), \
+			float(market.ticker(coinName)[0]["percent_change_24h"]))  
 	except:
-		coin = Coin(coinInfo, 0, 0)
+		coin = Coin(coinInfo, float(coinAmount), 0, 0, 0)
 	coins.append(coin)
 	total = total + coin.worth
 	
@@ -72,26 +91,41 @@ th, td {
     padding: 5px;
     text-align: left;
 }
+.green_cell {
+    color: green;
+}
+.red_cell {
+    color: red;
+}
 </style>"""
 f.write(table_style + '\n')
-f.write('<table>\n<tr>\n\t<th>Coin</th>\n \t<th>Coin price</th>\n \t<th>Coin amount</th>\n \t<th>Coin worth</th>\n')
+f.write('<table>\n<tr>\n\t<th>Coin</th>\n \t<th>Price</th>\n \t<th>Change in 1 hour</th>\n \t<th>Change in 24 hour</th>\n \t<th>Amount</th>\n \t<th>Worth</th>\n')
 
 i = 0
 for coin in sorted_coins:
 	i = i + 1
 	name = "%d. %s" % (i, coin.name.upper())
 	f.write('<tr>\n')
-	if (coin.price_usd != 0):
-		price = "$%0.3f (%0.6f BTC)" % (coin.price_usd, coin.price_usd / btc_usd)
+	if (coin.price_cur != 0):
+		price = "%0.3f %s (%0.6f BTC)" % (coin.price_cur, CURRENCY.upper(), coin.price_cur / btc_ratio)
 		amount = "%0.3f" % coin.amount
-		worth = "$%0.3f (%0.6f BTC)" % (coin.worth, coin.worth / btc_usd) 
-		f.write("\t<td>%s%s</td>\n \t<td>%s</td>\n \t<td>%s</td>\n \t<td>%s</td>\n" % (name, imageStr(coin.name), price, amount, worth))
+		worth = "%0.3f %s (%0.6f BTC)" % (coin.worth, CURRENCY.upper(), coin.worth / btc_ratio) 
+		change1h = "<td class=\"%s\">%0.2f%%</td>" % ("green_cell" if (coin.change1h >= 0) else "red_cell", coin.change1h)
+		change24h = "<td class=\"%s\">%0.2f%%</td>" % ("green_cell" if (coin.change24h >= 0) else "red_cell", coin.change24h)
+		f.write("\t<td>%s%s</td>\n \t<td>%s</td>\n \t%s\n \t%s\n \t<td>%s</td>\n \t<td>%s</td>\n" % \
+			(name, imageStr(coin.name), price, change1h, change24h, amount, worth))
 	else:
-		err_msg = "|This coin doesn't exist"
-		f.write("<td>\n%-25s</td><td>\n%s</td>\n" % (name, err_msg))
+		price = "This coin doesn't exist" 
+		amount = "%0.3f" % coin.amount
+		worth = "-" 
+		change1h = "<td>-</td>"
+		change24h = "<td>-</td>"
+		f.write("\t<td>%s%s</td>\n \t<td>%s</td>\n \t%s\n \t%s\n \t<td>%s</td>\n \t<td>%s</td>\n" % \
+			(name, imageStr(coin.name), price, change1h, change24h, amount, worth))
 	f.write('</tr>\n\n')
 f.write('</table>\n\n')
-f.write("<p>Total = <b>$%.2f (%.6f BTC)</b></p>" % (total, total / btc_usd))
+total_worth = "<p>Total = <b>%.2f %s (%.6f BTC)</b></p>" % (total, CURRENCY.upper(), total / btc_ratio)
+f.write(total_worth)
 f.write('</body>\n</html>\n')
 f.close()
 
