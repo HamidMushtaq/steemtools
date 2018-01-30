3ac7e06b246dc2c1218163500561d67d7c347fce
HamidMushtaq <hamidmushtaq@yahoo.com>
2018-01-30 07:44:02 +0100

    * Improved gui. Now also includes changes per hour and day. * Added support for other currencies besides USD * Added hyperlinks to coinmarketcap for coins * Removed dependency on coinmarketcap api

--- coinsworth_html/coinsworth_html.py
+++ coinsworth_html/coinsworth_html.py
@@ -1,9 +1,7 @@
 # This program calculates the total worth of all your coins
-# Before running it, install the coinmarket api -> pip install coinmarketcap
 # All your coins with their amounts should be put in a file coins.txt. An example file is given in this folder.
 # Author: Hamid Mushtaq
 import time
-import coinmarketcap
 import json, requests
 import operator
 import datetime
@@ -29,16 +27,8 @@ def imageStr(coinInfo):
 	else:
 		return ""
 		
-def getCurrencySymbol():
-	for currency in currencies:
-		print currency + '-' + CURRENCY
-		if currency == CURRENCY:
-			return currencies[CURRENCY]
-	return "$" # Dollar by default
-	
-market = coinmarketcap.Market()
 # If no currency is given in coins.txt, USD would be assumed as default
-btc_ratio = float(market.ticker("bitcoin", convert=CURRENCY)[0]["price_usd"]) 
+btc_ratio = float(requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/').json()[0]["price_usd"])
 
 filepath = 'coins.txt'  
 coins = []
@@ -54,14 +44,21 @@ for line in lines:
 	coinInfo = x[0].strip()
 	if coinInfo.lower() == "currency":
 		CURRENCY = x[1].strip().lower()
-		btc_ratio = float(market.ticker("bitcoin", convert=CURRENCY)[0]["price_" + CURRENCY])
+		if CURRENCY.lower() != 'usd':
+			try:
+				btc_ratio = float(requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=' + CURRENCY).json()[0]["price_" + CURRENCY])
+			except:
+				CURRENCY = 'usd' # When an invalid currency is entered, revert back to the default usd.
 		continue
 	coinName = coinInfo.split(':')[0]
 	coinAmount = x[1].strip()
 	try:
-		coin = Coin(coinInfo, float(coinAmount), float(market.ticker(coinName, convert=CURRENCY)[0]["price_" + CURRENCY]), \
-			float(market.ticker(coinName)[0]["percent_change_1h"]), \
-			float(market.ticker(coinName)[0]["percent_change_24h"]))  
+		req_str = 'https://api.coinmarketcap.com/v1/ticker/' + coinName + '/?convert=' + CURRENCY
+		rj = requests.get(req_str).json()[0]
+		price = rj["price_" + CURRENCY]
+		change1h = rj["percent_change_1h"]
+		change24h = rj["percent_change_24h"]
+		coin = Coin(coinInfo, float(coinAmount), float(price), float(change1h), float(change24h))  
 	except:
 		coin = Coin(coinInfo, float(coinAmount), 0, 0, 0)
 	coins.append(coin)
@@ -80,36 +77,56 @@ f = open(html_fname, 'w')
 f.write('<!DOCTYPE html>\n<html>\n<body>\n\n')
 table_style="""
 <style>
-table {
-    width:100%;
-}
-table, th, td {
-    border: 1px solid black;
+#cointable {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
     border-collapse: collapse;
+    width: 100%;
 }
-th, td {
-    padding: 5px;
+
+#cointable td, #cointable th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#cointable tr:nth-child(even){background-color: #f2f2f2;}
+
+#cointable th {
+    padding-top: 12px;
+    padding-bottom: 12px;
     text-align: left;
+    background-color: darkblue;
+    color: white;
 }
+
+h2 {
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+
 .green_cell {
     color: green;
 }
 .red_cell {
     color: red;
 }
+colored_span {
+	color: darkblue 
+}
 </style>"""
 f.write(table_style + '\n')
-f.write('<table>\n<tr>\n\t<th>Coin</th>\n \t<th>Price</th>\n \t<th>Change in 1 hour</th>\n \t<th>Change in 24 hour</th>\n \t<th>Amount</th>\n \t<th>Worth</th>\n')
+
+f.write('<h2>Total worth = <colored_span>%.2f %s (%.4f BTC)</colored_span></h2>\n' % (total, CURRENCY.upper(), total / btc_ratio))
 
 i = 0
+f.write('<table id = cointable>\n<tr>\n\t<th>Coin</th>\n \t<th>Price</th>\n \t<th>Change in 1 hour</th>\n \t<th>Change in 24 hours</th>\n \t<th>Amount</th>\n \t<th>Worth</th>\n')
 for coin in sorted_coins:
 	i = i + 1
-	name = "%d. %s" % (i, coin.name.upper())
+	name = "%d. <a href=\"https://coinmarketcap.com/currencies/%s/\" target=\"_blank\">%s</a>" % \
+		(i, coin.name.split(':')[0].lower(), coin.name.upper())
 	f.write('<tr>\n')
 	if (coin.price_cur != 0):
-		price = "%0.3f %s (%0.6f BTC)" % (coin.price_cur, CURRENCY.upper(), coin.price_cur / btc_ratio)
+		price = "%0.3f %s (%0.8f BTC)" % (coin.price_cur, CURRENCY.upper(), coin.price_cur / btc_ratio)
 		amount = "%0.3f" % coin.amount
-		worth = "%0.3f %s (%0.6f BTC)" % (coin.worth, CURRENCY.upper(), coin.worth / btc_ratio) 
+		worth = "%0.3f %s (%0.4f BTC)" % (coin.worth, CURRENCY.upper(), coin.worth / btc_ratio) 
 		change1h = "<td class=\"%s\">%0.2f%%</td>" % ("green_cell" if (coin.change1h >= 0) else "red_cell", coin.change1h)
 		change24h = "<td class=\"%s\">%0.2f%%</td>" % ("green_cell" if (coin.change24h >= 0) else "red_cell", coin.change24h)
 		f.write("\t<td>%s%s</td>\n \t<td>%s</td>\n \t%s\n \t%s\n \t<td>%s</td>\n \t<td>%s</td>\n" % \
@@ -124,8 +141,6 @@ for coin in sorted_coins:
 			(name, imageStr(coin.name), price, change1h, change24h, amount, worth))
 	f.write('</tr>\n\n')
 f.write('</table>\n\n')
-total_worth = "<p>Total = <b>%.2f %s (%.6f BTC)</b></p>" % (total, CURRENCY.upper(), total / btc_ratio)
-f.write(total_worth)
 f.write('</body>\n</html>\n')
 f.close()
 
